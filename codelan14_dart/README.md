6a4c511c58cee01065252befe419d97038d86af2
Afif-Ghifari <aghifari1204@gmail.com>
2025-11-30 11:50:23 +0700

    W14: Soal 1

---
+++ codelan14_dart/README.md
@@ -0,0 +1,29 @@
+# 14 | RESTful API
+
+| Nama:   | Muhammad Afif Al Ghifari |
+|---------|--------------------------|
+| Kelas:  | TI-3H                    |
+| NIM:    | 2341720168               |
+| Absen   | 19                       |
+
+## Praktikum 1
+### Soal 1
+- Tambahkan nama panggilan Anda pada title app sebagai identitas hasil pekerjaan Anda.
+- Gantilah warna tema aplikasi sesuai kesukaan Anda.
+<img src="img/prak1-1b.png">
+- Capture hasil aplikasi Anda, lalu masukkan ke laporan di README dan lakukan commit hasil jawaban Soal 1 dengan pesan "W14: Jawaban Soal 1"
+<img src="img/prak1-1a.png">
+
+## Praktikum 2
+### Soal 2
+- Tambahkan field baru dalam JSON maupun POST ke Wiremock!
+- Capture hasil aplikasi Anda berupa GIF di README dan lakukan commit hasil jawaban Soal 2 dengan pesan "W14: Jawaban Soal 2"
+
+## Praktikum 3
+### Soal 3
+- Ubah salah satu data dengan Nama dan NIM Anda, lalu perhatikan hasilnya di Wiremock.
+- Capture hasil aplikasi Anda berupa GIF di README dan lakukan commit hasil jawaban Soal 3 dengan pesan "W14: Jawaban Soal 3"
+
+## Praktikum 4
+### Soal 4
+- Capture hasil aplikasi Anda berupa GIF di README dan lakukan commit hasil jawaban Soal 4 dengan pesan "W14: Jawaban Soal 4"
---
+++ codelan14_dart/store_data_afif/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import './model/pizza.dart';
+import './httphelper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String pizzaString = "";
+  List<Pizza> myPizzas = [];
+
+  Future<List<Pizza>> callPizzas() async {
+    HttpHelper helper = HttpHelper();
+    List<Pizza> pizzas = await helper.getPizzaList();
+    return pizzas;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('JSON Afif'),
+        titleTextStyle: const TextStyle(color: Colors.white),
+        backgroundColor: Colors.green,
+      ),
+      body: FutureBuilder(
+        future: callPizzas(),
+        builder: (BuildContext context, AsyncSnapshot<List<Pizza>> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data == null) ? 0 : snapshot.data!.length,
+            itemBuilder: (BuildContext context, int position) {
+              return ListTile(
+                title: Text(snapshot.data![position].pizzaName),
+                subtitle: Text(
+                  snapshot.data![position].description +
+                      ' - € ' +
+                      snapshot.data![position].price.toString(),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
